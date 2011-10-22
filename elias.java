35f3fcd65242959ef270616b3ff7c8f71061aff0
Daniel Cussen <daniel.cussen@gmail.com>
2011-10-22 13:08:16 -0700

    Lots of changes, taking input

--- elias.java
+++ elias.java
@@ -1,9 +1,56 @@
 import java.io.*;
-
+import java.io.*;
+class U { //en archivo U.java
+    //declaracion de entrada estandar (teclado)
+    static public BufferedReader teclado =
+	new BufferedReader(new InputStreamReader(System.in));
+    //funciones (metodos) para leer enteros y reales
+    static public String readLine(String x)throws IOException{
+	print(x); return teclado.readLine();
+    }
+    static public int readInt(String x)throws IOException{
+	return Integer.parseInt(readLine(x));
+    }
+    static public int readInt()throws IOException{
+	return readInt("");
+    }
+    static public double readDouble(String x)throws IOException{
+	return Double.parseDouble(readLine(x));
+    }
+    static public double readDouble()throws IOException{
+	return readDouble("");
+    }
+    //metodos para escribir frases, enteros y reales
+    static public void print(String x){
+	System.out.print(x);
+    }
+    static public void print(int x){
+	System.out.print(x);
+    }
+    static public void print(double x){
+	System.out.print(x);
+    }
+    static public void println(String x){
+	System.out.println(x);
+    }
+    static public void println(int x){
+	System.out.println(x);
+    }
+    static public void println(double x){
+	System.out.println(x);
+    }
+}//fin de clase U
 public class elias{
     public static void main(String[] args){
-	System.out.println(""+encodeInt(22));
+	try{
+	    int input = U.readInt("Integer: ");
+	    System.out.println(""+encodeInt(65807));
+	}
+	catch(java.io.IOException exp){
+	    exp.printStackTrace();
+	}
     }
+	//
     public static int loglen(int n){
 	int i = 0;
 	while (n > 0){
@@ -23,8 +70,7 @@ public class elias{
     }
     public static String encodeInt(int n){
 	String str = "", escaper = "0";
-	int i = 0, index = 1, payload = n, y = 4;
-	payload++;
+	int i = 0, index = 1, payload = n+1, y = 4;
 	while(payload >= y){
 	    payload++;
 	    payload -= y;
@@ -33,9 +79,9 @@ public class elias{
 	}
 	index = loglen(payload);
 	String indexstr = (""+binary(index));
-	for(int j=indexstr.length(); j<escaper.length(); j++)
+	for(int j=indexstr.length();j<escaper.length();j++)
 	    indexstr = "0"+indexstr;
-	str=str.concat(escaper+" "+indexstr);
+	str = str.concat(escaper+" "+indexstr);
 	if(index>0)
 	    str = str.concat(" "+binary(payload));
 	return str;
