ed5113c3185b4091dfe0809314cf73be737cb544
Daniel Cussen <daniel.cussen@gmail.com>
2011-10-23 15:13:57 -0700

    Apparently bug free

--- elias.java
+++ elias.java
@@ -23,7 +23,7 @@ public class elias{
     public static int[] payload(int n){
 	int count = 1, payload = n, y = 4, b = 3, prev = 0;
 	int vec[] = new int[3];
-	while(payload >= b){ //perhaps this loop should be unrolled since it will only operate on ints
+	while(payload >= b){ //perhaps this loop should be unrolled since it will only operate on 32-bit ints
 	    prev = b;
 	    y *= y;
 	    b += y;
@@ -33,7 +33,11 @@ public class elias{
 	payload -= prev;
 	vec[0]=count;
 	vec[1]=loglen(payload);
-	vec[2]=payload;
+	if(vec[1]!=0)
+	    vec[2]=(payload+1)%((1<<vec[1])/2);
+	else
+	    vec[2]=0;
+	vec[1]--;
 	return vec;
     }
     public static String binary(int n){
@@ -54,51 +58,7 @@ public class elias{
     }
     public static String encodeInt(int n){
 	int payload[] = payload(n);
-	//	return ""+payload[0]+" "+binary(payload[1])+" "+payload[2];
-	return ""+payload[2];
-	//	return ""+padding(payload[0],"0",'1')+" "+padding(payload[0],binary(payload[1]),'0')+
-	//  " "+padding(payload[1],binary(payload[2]),'0');
+	return ""+padding(payload[0],"0",'1')+padding(payload[0],binary(payload[1]),'0')+
+	  padding(payload[1],binary(payload[2]),'0');
     }
 }
-
-
- 0  0
- 0  1 0
- 0  1 1
-10 00
-10 01 0
-10 01 1
-10 10 00
-10 10 01
-10 10 10
-10 10 11
-10 11 000
-10 11 001
-10 11 010
-10 11 011
-10 11 100
-10 11 101
-10 11 110
-10 11 111
-
-3
-15
-255
-65535
-
-1
-2
-
-1
-2
-4
-8
-
-1
-2
-4
-8
-16
-32
-64
-128
